6b490d9398afa8d73c54b88e131efa763c7bb564
ktustanowski <kamiltustanowski@macbook-pro-kamil-2.local>
2016-04-07 10:10:08 +0200

    Initial Commit

---
+++ FeatureToggle-Proof-Of-Concept/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FeatureToggle-Proof-Of-Concept
+//
+//  Created by Kamil Tustanowski on 07.04.2016.
+//  Copyright © 2016 ktustanowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
